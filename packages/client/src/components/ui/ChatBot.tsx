7bb347210515ebd9eed32ac0223e84031c5ad67d
ratnadeepnayak <ratnadeepnayak@gmail.com>
2026-04-15 19:53:20 -0400

    feat: enhance ChatBot component with form handling using react-hook-form and axios for API integration

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -1,19 +1,53 @@
+import { useRef, type KeyboardEvent } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import { ArrowUp } from 'lucide-react';
-
 import { Button } from './button';
 
+type FormData = {
+   prompt: string;
+};
+
 const ChatBot = () => {
+   const conversationId = useRef(crypto.randomUUID());
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+   const onSubmit = async ({ prompt }: FormData) => {
+      reset();
+
+      const { data } = await axios.post('/api/chat', {
+         prompt,
+         conversationId: conversationId.current,
+      });
+      console.log(data);
+   };
+
+   const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault();
+         handleSubmit(onSubmit)();
+      }
+   };
+
    return (
-      <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+      <form
+         onSubmit={handleSubmit(onSubmit)}
+         onKeyDown={onKeyDown}
+         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+      >
          <textarea
+            {...register('prompt', {
+               required: true,
+               validate: (data) => data.trim().length > 0,
+            })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask anything"
             maxLength={1000}
          />
-         <Button className="rounded-full w-9 h-9">
+         <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
             <ArrowUp />
          </Button>
-      </div>
+      </form>
    );
 };
 
